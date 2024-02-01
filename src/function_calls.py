927f11e1a755a707eb0f6a850c78b8fe29e3804a
Ezzeri Esa <ezzeri.esa@gmail.com>
2024-02-01 08:49:01 -0800

    minimal example with individual decorators

--- src/function_calls.py
+++ src/function_calls.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict
 import functools
-
 import pydantic
 
 
@@ -15,4 +14,30 @@ class OpenAISchema(pydantic.BaseModel):
 def openai_schema(cls: Any) -> OpenAISchema:
     return functools.wraps(cls, updated=())(
         pydantic.create_model(cls.__name__, __base__=(cls, OpenAISchema))
+    )  # type: ignore[return-value]
+
+
+class OpenAIMethodSchema(pydantic.BaseModel):
+    @classmethod
+    def openai_method_schema(cls) -> Dict[str, Any]:
+        name = cls.model_json_schema()["title"]
+        return {"name": name}
+
+
+def openai_method_schema(cls: Any) -> OpenAIMethodSchema:
+    return functools.wraps(cls, updated=())(
+        pydantic.create_model(cls.__name__, __base__=(cls, OpenAIMethodSchema))
+    )  # type: ignore[return-value]
+
+
+class OpenAIPropertySchema(pydantic.BaseModel):
+    @classmethod
+    def openai_property_schema(cls) -> Dict[str, Any]:
+        name = cls.model_json_schema()["title"]
+        return {"name": name}
+
+
+def openai_property_schema(cls: Any) -> OpenAIPropertySchema:
+    return functools.wraps(cls, updated=())(
+        pydantic.create_model(cls.__name__, __base__=(cls, OpenAIPropertySchema))
     )  # type: ignore[return-value]
--- src/test_function_calls.py
+++ src/test_function_calls.py
@@ -1,4 +1,5 @@
 import pydantic
+import types
 
 import function_calls
 
@@ -10,3 +11,21 @@ def test_openai_schema() -> None:
         columns: str
 
     assert isinstance(DataFrame.openai_schema, dict)  # type: ignore[attr-defined]
+
+
+def test_openai_method_schema() -> None:
+    @function_calls.openai_method_schema
+    class DataFrame(pydantic.BaseModel):
+        data: str
+        columns: str
+
+    assert isinstance(DataFrame.openai_method_schema, types.MethodType)  # type: ignore[attr-defined]
+
+
+def test_openai_property_schema() -> None:
+    @function_calls.openai_property_schema
+    class DataFrame(pydantic.BaseModel):
+        data: str
+        columns: str
+
+    assert isinstance(DataFrame.openai_property_schema, types.MethodType)  # type: ignore[attr-defined]
